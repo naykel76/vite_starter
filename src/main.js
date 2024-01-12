f0025928bfde4aeb7f8df181a709a72b6ba812c5
Nathan Watts <nathan@naykel.com.au>
2024-01-13 07:55:47 +1000

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './jtb.scss'
+import logo from '/logo.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+<div class="flex items-center justify-center h-screen">
+    <div class="tac">
+        <img src="${logo}" class="mx-auto h-5" alt="Naykel logo" />
+        <div class="my"><h1 class="my">Vite Starter with JTB</h1></div>
+        <button class="btn" id="counter" type="button"></button>
+    </div>
+</div>
+`
+
+setupCounter(document.querySelector('#counter'))
